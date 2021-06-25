a1fef575f22c61158adf4679c02e408bbbd0a417
larry <xspanni@gmail.com>
2021-06-25 17:06:33 +0800

    添加了一个clion项目，用以演示如何使用library项目所生成的公共动态库

--- projects/everquest-library/README.md
+++ projects/everquest-library/README.md
@@ -1,3 +1,8 @@
-出于学习kotlin目的而创建的项目
+出于学习kotlin multiplatform目的而创建的项目
 
-示例代码在example目录下
+
+### 生成C动态库
+将会在```build/bin/native/debugShared``目录下生成目标
+```shell
+./gradlew linkNative
+```
--- projects/everquest-library/build.gradle.kts
+++ projects/everquest-library/build.gradle.kts
@@ -35,11 +35,24 @@ kotlin {
 
         }
     }
+
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
+        hostOs == "Mac OS X" -> macosX64("native") {
+            binaries {
+                sharedLib {
+                    baseName = "native"
+                }
+            }
+        }
+        hostOs == "Linux" -> linuxX64("native") {
+            binaries {
+                sharedLib {
+                    baseName = "native"
+                }
+            }
+        }
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
@@ -64,4 +77,8 @@ kotlin {
         val nativeMain by getting
         val nativeTest by getting
     }
+}
+
+tasks.wrapper {
+    distributionType = Wrapper.DistributionType.ALL
 }
